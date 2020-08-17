b3659086d13a51ce3d82eb3837ffba3dfea582b2
Luis Salazar <bp.lusv@gmail.com>
2020-08-17 11:38:29 -0600

    Add Unit tests and get_questions action

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -12,10 +12,12 @@ class TriviaTestCase(unittest.TestCase):
 
     def setUp(self):
         """Define test variables and initialize app."""
+
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}/{}".format('localhost:5432',
+                                                         self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,15 +26,25 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
-        """Executed after reach test"""
+        """Executed after each test"""
         pass
 
-    """
-    TODO
-    Write at least one test for each test for successful operation and for expected errors.
-    """
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        body = res.get_json()
+        self.assertTrue(body)
+        self.assertEqual(res.status_code, 200)
+
+    def test_get_questions(self):
+        res = self.client().get('/questions?page=1')
+        body = res.get_json()
+        self.assertTrue(body['questions'])
+        self.assertTrue(body['total_questions'])
+        self.assertTrue(body['categories'])
+        self.assertTrue(body['current_category'])
+        self.assertEqual(res.status_code, 200)
 
 
 # Make the tests conveniently executable
